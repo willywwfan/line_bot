0acda27a1df85e64305965ad53543e724e9e7f96
alice.yurulin <alice.yurulin@gmail.com>
2022-04-14 20:15:24 +0800

    text optimization

--- lambda_function.py
+++ lambda_function.py
@@ -73,7 +73,7 @@ class PostgresBaseManager:
             self.last3 = self.last3[1:] + [datas]
             # print("Data row = " + datas )
         
-        self.last3 = self.last3[0] + "\n" + self.last3[1] + "\n" + self.last3[2] + "\n"
+        self.last3 = self.last3[0] + "\n\t\n" + self.last3[1] + "\n\t\n" + self.last3[2]
 
 postgres_manager = PostgresBaseManager()
 

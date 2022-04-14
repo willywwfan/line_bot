1ce8543232c5cb023532f1db065575b138a9f082
alice.yurulin <alice.yurulin@gmail.com>
2022-04-14 20:06:30 +0800

    account update in lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,73 @@ from linebot.models import (
 
 import os
 import json
+import ssl
+
+import psycopg2
+from datetime import datetime
+
+# test
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Hello from Lambda!")
+  }
+
+# body
+class PostgresBaseManager:
+
+    def __init__(self):
+
+        self.database = 'd9r8va4pq94vp5'
+        self.user = 'vnnvynpuzipegu'
+        self.password = 'e17d5a6ec332f03123b57b0fddd18a7bc5dcbda5908853191d84f293bab4bd3c'
+        self.host = 'ec2-52-203-118-49.compute-1.amazonaws.com'
+        self.port = '5432'
+        self.conn = self.connectServerPostgresDb()
+
+    def connectServerPostgresDb(self):
+        """
+        :return: 連接 Heroku Postgres SQL 認證用
+        """
+        conn = psycopg2.connect(dbname=self.database,user=self.user,password=self.password,host=self.host,port=self.port,sslmode="require")
+        return conn
+
+    def closePostgresConnection(self):
+        """
+        :return: 關閉資料庫連線使用
+        """
+        self.conn.close()
+
+    def insert(self,text,num):
+        cur = self.conn.cursor()
+        date = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        cur.execute("INSERT INTO accounts_table (owner_name, text, amount, date) VALUES (%s, %s, %s, %s);", ("Willy", text, num, date))
+        self.conn.commit()
+
+        select = "SELECT MAX(record_no) FROM accounts_table"
+        cur.execute(select)
+        max_record_no = cur.fetchall()[0][0]
+        print(max_record_no)
+
+        limit = "DELETE FROM accounts_table WHERE record_no < " + str(max_record_no-19) + " RETURNING *;"
+        cur.execute(limit)
+        self.conn.commit()
+        deleted = cur.fetchall()
+        
+        cur.execute("SELECT * FROM accounts_table;")
+        rows = cur.fetchall()
+        self.last3 = [" "," "," "]
+        for row in rows:
+            datas = ""
+            for data in row:
+                datas = datas + str(data) + ", "
+            self.last3 = self.last3[1:] + [datas]
+            # print("Data row = " + datas )
+        
+        self.last3 = self.last3[0] + "\n" + self.last3[1] + "\n" + self.last3[2] + "\n"
+
+postgres_manager = PostgresBaseManager()
+
 
 line_bot_api = LineBotApi(os.environ['Channel_access_token'])
 handler = WebhookHandler(os.environ['Channel_secret'])
@@ -17,9 +84,19 @@ handler = WebhookHandler(os.environ['Channel_secret'])
 def lambda_handler(event, context):
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text))
+        if event.message.text[:2] == "記帳":
+            text = event.message.text
+            postgres_manager = PostgresBaseManager()
+            num = int(text.split(" ")[-1])
+            postgres_manager.insert(text,num)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="上三筆資料:\n" + postgres_manager.last3))
+                # TextSendMessage(text=event.message.text))
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text))
 
     # get X-Line-Signature header value
     signature = event['headers']['x-line-signature']
